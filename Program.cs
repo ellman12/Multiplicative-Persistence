7fe8ce0d5cd6c91686e8a5b541f0eb539bbcbc32
Elliott <ellduc4@gmail.com>
2022-04-08 21:01:39 -0500

    Added a new mode: Randomized. Now, instead of incrementing each time, just generate and try a randon BigInt

--- Program.cs
+++ Program.cs
@@ -1,57 +1,79 @@
-using System.Numerics;
+﻿using System.Numerics;
+
+byte mode; //0 for default (sequential), 1 for randomized.
+byte minRandLength, maxRandLength;
+
+Console.Write("Enter mode. 0 is default (sequential), 1 is randomized: ");
+mode = Convert.ToByte(Console.ReadLine());
 
 Console.Write("How many threads? ");
 int numThreads = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Starting Number? ");
-string startNumStr = Console.ReadLine()!;
-string dividerLine = "----------" + new string('-', startNumStr.Length); //This start string accounts for the "Thread x: " text that is always printed. Also adds perfect amount of '-'.
-BigInteger startNum = BigInteger.Parse(startNumStr);
-
 Console.Write("Above what value should number of steps be printed? ");
 int interestedStepSize = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("How often to print current numbers of each thread (in ms)? ");
-int printDelay = Convert.ToInt32(Console.ReadLine());
-
-Thread[] threads = new Thread[numThreads];
 BigInteger[] currentNumbers = new BigInteger[numThreads];
+Thread[] threads = new Thread[numThreads];
 int[] stepsArray = new int[numThreads];
-for (int i = 0; i < threads.Length; i++)
-{
-	int ii = i;
-	stepsArray[i] = 0;
-	currentNumbers[i] = startNum + i;
-	threads[i] = new Thread(() => InfinitePersistence(ref currentNumbers[ii], ref stepsArray[ii], ii))
-	{
-		Priority = ThreadPriority.Highest,
-		IsBackground = false
-	};
-	threads[i].Start();
-}
 
-while (true)
+if (mode == 0)
 {
-	Console.WriteLine($"{DateTime.Now:h:mm:ss tt} Current Numbers: ");
-	for (int i = 0; i < currentNumbers.Length; i++)
+	Console.Write("Starting Number? ");
+	string startNumStr = Console.ReadLine()!;
+	string dividerLine = "----------" + new string('-', startNumStr.Length);
+	BigInteger startNum = BigInteger.Parse(startNumStr);
+
+	Console.Write("How often to print current numbers of each thread (in ms)? ");
+	int printDelay = Convert.ToInt32(Console.ReadLine());
+
+	for (int i = 0; i < threads.Length; i++)
 	{
-		Console.WriteLine($"Thread {i}: {currentNumbers[i]}\t");
+		int ii = i;
+		stepsArray[i] = 0;
+		currentNumbers[i] = startNum + i;
+		threads[i] = new Thread(() => InfinitePersistence(ref currentNumbers[ii], ref stepsArray[ii], ii))
+		{
+			Priority = ThreadPriority.Highest,
+			IsBackground = false
+		};
+		threads[i].Start();
 	}
-	Console.WriteLine(dividerLine);
-	Thread.Sleep(printDelay);
-}
 
-void InfinitePersistence(ref BigInteger currentNumber, ref int steps, int threadNum)
-{
 	while (true)
 	{
-		Persistence(currentNumber, ref steps);
-		currentNumber++;
-		if (steps > interestedStepSize) Console.WriteLine($"Thread {threadNum}: {currentNumber}\tSteps: {steps}\t{DateTime.Now.ToString("hh:mm:ss tt")}");
-		steps = 0;
+		Console.WriteLine($"{DateTime.Now:h:mm:ss tt} Current Numbers: ");
+		for (int i = 0; i < currentNumbers.Length; i++)
+		{
+			Console.WriteLine($"Thread {i}: {currentNumbers[i]}\t");
+		}
+
+		Console.WriteLine(dividerLine);
+		Thread.Sleep(printDelay);
+	}
+}
+else if (mode == 1)
+{
+	Console.Write("Enter min random number length: ");
+	minRandLength = Convert.ToByte(Console.ReadLine());
+	Console.Write("Enter max random number length: ");
+	maxRandLength = Convert.ToByte(Console.ReadLine());
+
+	if (minRandLength >= maxRandLength) return;
+
+	for (int i = 0; i < threads.Length; i++)
+	{
+		int ii = i;
+		stepsArray[i] = 0;
+		threads[i] = new Thread(() => InfiniteGenAndCheck(ref stepsArray[ii], ii))
+		{
+			Priority = ThreadPriority.Highest,
+			IsBackground = false
+		};
+		threads[i].Start();
 	}
 }
 
+//Runs Multiplicative Persistence on a single BigInteger.
 void Persistence(BigInteger bigInt, ref int steps)
 {
 	string num = Convert.ToString(bigInt)!;
@@ -66,4 +88,39 @@ void Persistence(BigInteger bigInt, ref int steps)
 		num = Convert.ToString(result)!;
 		steps += 1;
 	}
+}
+
+//What each Thread runs when running in mode 0 (sequential).
+void InfinitePersistence(ref BigInteger currentNumber, ref int steps, int threadNum)
+{
+	while (true)
+	{
+		Persistence(currentNumber, ref steps);
+		currentNumber++;
+		if (steps > interestedStepSize) Console.WriteLine($"Thread {threadNum}: {currentNumber}\tSteps: {steps}\t{DateTime.Now.ToString("hh:mm:ss tt")}");
+		steps = 0;
+	}
+}
+
+//What each Thread runs in mode 1. Loops forever generating BigIntegers of length defined at top of file, and runs Persistence() on it each time.
+void InfiniteGenAndCheck(ref int steps, int threadNum)
+{
+	Random random = new();
+	while (true)
+	{
+		BigInteger currentNumber = BigInteger.Abs(GetRandomBigInt(random.Next(minRandLength, maxRandLength)));
+		Persistence(currentNumber, ref steps);
+		if (steps > interestedStepSize) Console.WriteLine($"Thread {threadNum}: {currentNumber}\tSteps: {steps}\t{DateTime.Now.ToString("hh:mm:ss tt")}");
+		steps = 0;
+	}
+}
+
+//https://stackoverflow.com/a/17357822
+//Used for mode 1.
+BigInteger GetRandomBigInt(int length)
+{
+	Random random = new();
+	byte[] data = new byte[length];
+	random.NextBytes(data);
+	return new BigInteger(data);
 }
